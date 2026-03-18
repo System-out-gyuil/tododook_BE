3fff5580eb3be5cafec10395f2864792f2f5e6bf
dev_gyuil <a46884334@gmail.com>
2026-03-18 15:43:03 +0900

    [test] terraform

--- src/test/java/com/gyul/tododook/TododookApplicationTests.java
+++ src/test/java/com/gyul/tododook/TododookApplicationTests.java
@@ -2,8 +2,11 @@ package com.gyul.tododook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+
+@ActiveProfiles("Test") // 빌드할때 테스트시에만 작동하도록 명시
 class TododookApplicationTests {
 
 	@Test
