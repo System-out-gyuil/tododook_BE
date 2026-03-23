dcce4d2963de7331496fb907787d5a4b87c3d71d
dev_gyuil <a46884334@gmail.com>
2026-03-23 15:53:30 +0900

    [test] deploy test

--- src/main/java/com/gyul/tododook/global/controller/GlobalController.java
+++ src/main/java/com/gyul/tododook/global/controller/GlobalController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GlobalController {
     @GetMapping("/health")
     public String health() {
+        System.out.println("Health check OK");
         return "ok";
     }
 }
