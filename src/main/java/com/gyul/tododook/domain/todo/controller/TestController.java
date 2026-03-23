a3eb19f4a43cf7b0bd0b52aa3c76476215fcb325
dev_gyuil <a46884334@gmail.com>
2026-03-23 16:07:49 +0900

    [test] deploy test

--- src/main/java/com/gyul/tododook/domain/todo/controller/TestController.java
+++ src/main/java/com/gyul/tododook/domain/todo/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+//    test
     @GetMapping("/")
     public String home() {
         return "ok";
