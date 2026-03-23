9ebadacdaf70efe34a203b1d772a72a97e4cc440
dev_gyuil <a46884334@gmail.com>
2026-03-23 15:40:22 +0900

    [test] CI/CD test

---
+++ src/main/java/com/gyul/tododook/global/controller/GlobalController.java
@@ -0,0 +1,12 @@
+package com.gyul.tododook.global.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GlobalController {
+    @GetMapping("/health")
+    public String health() {
+        return "ok";
+    }
+}
